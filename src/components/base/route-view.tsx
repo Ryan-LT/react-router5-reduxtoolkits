362b6d008f0c871a2a7300cfc49ffdcf82644c6e
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-03-30 20:53:34 +0700

    chore(test): test typescript error check

--- src/components/base/route-view.tsx
+++ src/components/base/route-view.tsx
@@ -1,10 +1,8 @@
 import React, { FunctionComponent } from 'react';
-import { Route as RouteState } from '@router';
+// import { Route as RouteState } from '@router';
 import { useRoute } from 'react-router5';
 
-export const RouterView: FunctionComponent<{ routes: RouteState[] }> = ({
-  routes,
-}) => {
+export const RouterView: FunctionComponent<{ routes }> = ({ routes }) => {
   const route = useRoute();
   const { Component } = routes.find((r) => r.name === route.route.name) || {};
   return Component ? <Component /> : null;
