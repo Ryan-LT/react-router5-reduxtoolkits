6a5691710e4b5bd574ffe17efa201fefccc41a13
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-03-30 21:01:27 +0700

    chore(test): pre-commmit

--- src/components/base/route-view.tsx
+++ src/components/base/route-view.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from 'react';
-// import { Route as RouteState } from '@router';
+import { Route as RouteState } from '@router';
 import { useRoute } from 'react-router5';
 
-export const RouterView: FunctionComponent<{ routes }> = ({ routes }) => {
+export const RouterView: FunctionComponent<{ routes: RouteState[] }> = ({
+  routes,
+}) => {
   const route = useRoute();
   const { Component } = routes.find((r) => r.name === route.route.name) || {};
   return Component ? <Component /> : null;
