876d7e18faa8b161be562f7b0ddde0d542bde9e5
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-03-31 18:57:56 +0700

    test(detail): test detail page

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -6,17 +6,39 @@ import { increment, decrement, State, testNavigateInRedux } from '@store';
 const Detail: FunctionComponent = () => {
   const dispatch = useDispatch();
   const counterValue = useSelector((state: State) => state.counter.value);
+
+  const delayCount = (): void => {
+    setTimeout(() => {
+      dispatch(increment());
+    }, 500);
+  };
+
   return (
     <div>
-      <button type="button" onClick={() => dispatch(increment())}>
+      <button
+        type="button"
+        data-testid="increase-button"
+        onClick={() => dispatch(increment())}
+      >
         Increase
       </button>
-      <h1>{counterValue}</h1>
-      <button type="button" onClick={() => dispatch(decrement())}>
+      <h1 data-testid="counter-value">{counterValue}</h1>
+      <button
+        type="button"
+        data-testid="decrease-button"
+        onClick={() => dispatch(decrement())}
+      >
         Decrease
       </button>
       <button type="button" onClick={() => dispatch(testNavigateInRedux())}>
         Navigate Back
+      </button>
+      <button
+        type="button"
+        data-testid="incease-async-button"
+        onClick={() => delayCount()}
+      >
+        Async Increase
       </button>
     </div>
   );
--- tests/pages/detail.test.tsx
+++ tests/pages/detail.test.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import { Detail } from '@pages';
-import { cleanup, render } from '../render-test';
+import { fireEvent } from '@testing-library/dom';
+import { cleanup, render, screen } from '../render-test';
 import '@testing-library/jest-dom';
 
 afterEach(cleanup);
+
+it('should take a snapshot', () => {
+  const { asFragment } = render(<Detail />);
+
+  expect(asFragment()).toMatchSnapshot();
+});
 
 describe('<Detail/>', () => {
   it('render Detail component', () => {
     const component = render(<Detail />);
     expect(component).toBeTruthy();
+  });
+
+  it('should be default 0', () => {
+    const { getByTestId } = render(<Detail />);
+    expect(getByTestId('counter-value')).toBeEnabled();
+    expect(getByTestId('counter-value')).toHaveTextContent('0');
+  });
+
+  it('increase counter', () => {
+    const { getByTestId } = render(<Detail />);
+    fireEvent.click(getByTestId('increase-button'));
+    expect(getByTestId('counter-value')).toHaveTextContent('1');
+  });
+
+  it('decrease counter', () => {
+    const { getByTestId } = render(<Detail />);
+    fireEvent.click(getByTestId('decrease-button'));
+    expect(getByTestId('counter-value')).toHaveTextContent('0');
+  });
+
+  it('decrease counter async', async () => {
+    const component = render(<Detail />);
+    const button = component.getByTestId('incease-async-button');
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    const counter = await component.findByText('1');
+    expect(counter).toHaveTextContent('4');
   });
 });
